70b63cbaf3fdf2e9e94d77837faa60bf562d2fe9
Mohamad anas fattoum <samaanas30@gmail.com>
2025-01-29 18:45:09 +0100

    add mein apps in settings

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # installed apps
+    'rest_framework',
+
+    # meine Apps
+
+    'bücherapi',
 ]
 
 MIDDLEWARE = [
