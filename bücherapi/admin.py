641e97e4525d3eafe3d808e71f93ac44a015e94e
Mohamad anas fattoum <samaanas30@gmail.com>
2025-01-29 20:15:07 +0100

    fiexed den admin.py um mehrere Autorien für einem buch zu wählen

--- bücherapi/admin.py
+++ bücherapi/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Buch, Autor
 
-class BookAdmin(admin.ModelAdmin):
-    filter_horizontal = 'authors' # Ermöglicht das einfache Hinzufügen von Autoren
+class BuchAdmin(admin.ModelAdmin):
+    filter_horizontal = ('autors',) # Ermöglicht das einfache Hinzufügen von Autoren
 
-admin.site.register([Buch])
-admin.site.register([Autor])
-# Register your models here.
+
+admin.site.register(Autor)
+admin.site.register(Buch, BuchAdmin)
